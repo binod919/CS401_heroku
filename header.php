0931d97f98940aadbca26d898daee0c9bd42ff49
Binod Karki <karki.binod919@gmail.com>
2019-02-20 13:43:44 -0700

    updating changes with index

--- header.php
+++ header.php
@@ -6,11 +6,11 @@ echo '<html>
 	<link rel="stylesheet" href="header.css">
 </head>
 <div class="header">
-	<img src="Logo.png" alt = "Logo" class="logo"> 
+	<img src="Logo.png" alt = "Logo" class="logo">
 	<div class="header-right">
 		<a class="active" href="home.php"> Home </a>
 		<a href="signup.php"> Sign Up </a>
-    <a href="login.php">Login </a>
+    <a href="index.php">Login </a>
 		<input id="searchBox" type="text" placeholder="Search" name="search"> </input>
 		<button id="searchButton" type="submit" name="Search"> <i class="searchBox"> </i> </button>
 	</div>
