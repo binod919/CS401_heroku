e52b9b001378ee2a91bc7e17199841e3c4006b5e
Binod Karki <binodkarki@u.boisestate.edu>
2019-04-05 16:17:19 -0600

    working signup

--- signupHandler.php
+++ signupHandler.php
@@ -1,8 +1,6 @@
 <?php
 session_start();
 
-//require_once 'Dao.php';
-
 $username = $_POST['username'];
 $firstname = $_POST['firstname'];
 $lastname = $_POST['lastname'];
@@ -19,24 +17,18 @@ if ($password !== $rePassword){
 require_once 'validation.php';
 $val = new validation();
 $isPassValid = $val->validatePassword($password);
-//echo $isPassValid;
+
 if($isPassValid == 0){
   echo "in if";
   $_SESSION['message'] = "Password must be of at least 8 characters and Atleast one upper and one lower case letter\n";
   header("Location: signup.php");
   exit;
 }
-echo $username;
-echo $password;
-echo " in here";
 
 require_once 'Dao.php';
-echo " in here too";
 $dao = new Dao();
-//echo " inhere3";
-//$dao->createUser($username, $firstname, $lastname, $email, $password);
-echo "success";
-//header("Location: index.php");
+$dao->createUser($username, $firstname, $lastname, $email, $password);
+header("Location: index.php");
 exit;
 
 ?>
