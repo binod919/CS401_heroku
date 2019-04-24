68ff533ac9db362f4915202aee100d5c3e04f63d
Binod Karki <binodkarki@u.boisestate.edu>
2019-04-24 17:22:30 -0600

    logout alert!

--- header.php
+++ header.php
@@ -7,6 +7,9 @@
 
 <head>
 	<link rel="stylesheet" href="header.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+    </script>
+    <script src="js/logout.js"></script>
 </head>
 <div class="header">
 	<img src="Logo.png" alt = "Logo" class="logo">
@@ -17,17 +20,17 @@
 			echo '<a class="thisRef" href="home.php"> Home </a>
 			<a href="signup.php"> Sign Up </a>
 	    <a href="index.php">Login </a>
-			<a href="index.php"> Logout </a>';
+			<a class = "logout" href="index.php"> Logout </a>';
 		}else if(isset($_SESSION['thisPage']) && $_SESSION['thisPage']=== "signup"){
 			echo '<a class="active" href="home.php"> Home </a>
 			<a class = "thisRef" href="signup.php"> Sign Up </a>
 	    <a href="index.php">Login </a>
-			<a href="index.php"> Logout </a> ';
+			<a class = "logout" href="index.php"> Logout </a> ';
 		} else if(isset($_SESSION['thisPage']) && $_SESSION['thisPage']=== "login"){
 			echo '<a class="active" href="home.php"> Home </a>
 			<a href="signup.php"> Sign Up </a>
 			<a class = "thisRef" href="index.php">Login </a>
-			<a href="index.php"> Logout </a> ';
+			<a class = "logout" href="index.php"> Logout </a> ';
 		}else {
 
 		echo '<a class="active" href="home.php"> Home </a>
---
+++ js/logout.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+$(".logout").click(function(){
+  alert("You are logged out!")
+})
+});
