107ae316a2fb6aa2cfbf14217a14e66f150cfd93
Binod Karki <binodkarki@u.boisestate.edu>
2019-04-29 08:51:40 -0600

    footer fiexd to position

--- footer.php
+++ footer.php
@@ -4,7 +4,6 @@ echo '
 <head>
 	<link rel="stylesheet" href="footer.css">
   </head>
-	<hr>
 <div id= "footer" > Privacy  |  Policies  |  Contact Us  |
 Copyright &copy; 2019 Share My Writings. All rights reserved.
 </div>
